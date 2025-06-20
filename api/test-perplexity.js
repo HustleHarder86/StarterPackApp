2ec7063101bf9ea8fe829e9b6d86312d63e00814
HustleHarder86 <amy__ali@hotmail.com>
2025-06-20 15:38:03 -0400

    Update Perplexity model

--- api/test-perplexity.js
+++ api/test-perplexity.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      model: "llama-3-sonar-large-32k-online",
+      model: "llama-3-sonar-large-online",
       messages: [
         { role: "user", content: "What is ROI in real estate?" }
       ]
